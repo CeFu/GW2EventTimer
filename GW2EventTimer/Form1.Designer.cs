5a30ae1dc9d37264521949ceb82d19fe57fc21a8
CeFu <duke646@hotmail.com>
2014-04-25 17:12:30 -0600

    V0.1 - Asura
    +Changed Title
    +Added Menu (Options/Exit)
    +Added Status Strip
    +Added System Time
    
    Code is looking bad, but oh well.

--- GW2EventTimer/Form1.Designer.cs
+++ GW2EventTimer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GW2EventTimer
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,22 +30,157 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.BtnTimerSet = new System.Windows.Forms.Button();
+            this.LabelTime = new System.Windows.Forms.Label();
+            this.tbMins = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSystemTime = new System.Windows.Forms.ToolStripLabel();
+            this.ServerClientTimer = new System.Windows.Forms.Timer(this.components);
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // BtnTimerSet
+            // 
+            this.BtnTimerSet.Location = new System.Drawing.Point(216, 296);
+            this.BtnTimerSet.Name = "BtnTimerSet";
+            this.BtnTimerSet.Size = new System.Drawing.Size(75, 23);
+            this.BtnTimerSet.TabIndex = 0;
+            this.BtnTimerSet.Text = "Start";
+            this.BtnTimerSet.UseVisualStyleBackColor = true;
+            this.BtnTimerSet.Click += new System.EventHandler(this.BtnTimerSet_Click);
+            // 
+            // LabelTime
+            // 
+            this.LabelTime.AutoSize = true;
+            this.LabelTime.Location = new System.Drawing.Point(297, 301);
+            this.LabelTime.Name = "LabelTime";
+            this.LabelTime.Size = new System.Drawing.Size(34, 13);
+            this.LabelTime.TabIndex = 1;
+            this.LabelTime.Text = "Blank";
+            // 
+            // tbMins
+            // 
+            this.tbMins.Location = new System.Drawing.Point(110, 298);
+            this.tbMins.Name = "tbMins";
+            this.tbMins.Size = new System.Drawing.Size(100, 20);
+            this.tbMins.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 513);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(524, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripSystemTime,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(524, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(78, 22);
+            this.toolStripLabel1.Text = "System Time:";
+            // 
+            // toolStripSystemTime
+            // 
+            this.toolStripSystemTime.Name = "toolStripSystemTime";
+            this.toolStripSystemTime.Size = new System.Drawing.Size(120, 22);
+            this.toolStripSystemTime.Text = "Syncing System Time";
+            this.toolStripSystemTime.Click += new System.EventHandler(this.toolStripSystemTime_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(524, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem,
+            this.MenuExit});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem1.Text = "Menu";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // MenuExit
+            // 
+            this.MenuExit.Name = "MenuExit";
+            this.MenuExit.Size = new System.Drawing.Size(152, 22);
+            this.MenuExit.Text = "Exit";
+            this.MenuExit.Click += new System.EventHandler(this.MenuExit_Click);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(524, 535);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.tbMins);
+            this.Controls.Add(this.LabelTime);
+            this.Controls.Add(this.BtnTimerSet);
+            this.Name = "MainForm";
+            this.Text = "[Phi] World Boss Timers";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button BtnTimerSet;
+        private System.Windows.Forms.Label LabelTime;
+        private System.Windows.Forms.TextBox tbMins;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripSystemTime;
+        private System.Windows.Forms.Timer ServerClientTimer;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuExit;
     }
 }
 
--- GW2EventTimer/Form1.cs
+++ GW2EventTimer/Form1.cs
@@ -12,11 +12,61 @@ using System.Diagnostics;
 
 namespace GW2EventTimer
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        int counter = 10;
+
+        public MainForm()
         {
             InitializeComponent();
+            ServerClientTime();
+        }
+
+        private void ServerClientTime()
+        {
+            ServerClientTimer = new System.Windows.Forms.Timer();
+            ServerClientTimer.Tick += new EventHandler(ServerClientTimer_Tick);
+            ServerClientTimer.Interval = 1000;
+            ServerClientTimer.Start();
+        }
+
+        private void ServerClientTimer_Tick(object sender, EventArgs e)
+        {
+            toolStripSystemTime.Text = DateTime.Now.ToString("HH:mm:ss tt");
+        }
+
+        private void BtnTimerSet_Click(object sender, EventArgs e)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(counter);
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000;
+            timer1.Start();
+            string FinalTime = string.Format("Server restarting in {0:D2}:{1:D2}:{2:D2}", t.Hours, t.Minutes, t.Seconds);
+            LabelTime.Text = FinalTime;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(counter);
+            counter--;
+            if (counter == 0)
+            {
+                counter = 10;
+            }
+
+            string FinalTime = string.Format("Server restarting in {0:D2}:{1:D2}:{2:D2}", t.Hours, t.Minutes, t.Seconds);
+            LabelTime.Text = FinalTime;
+        }
+
+        private void toolStripSystemTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
